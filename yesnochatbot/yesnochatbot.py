f054dd5fcd25481961e5ed3b86a6cbf7063e72ec
daniel9-hub <dnlbilko@gmail.com>
2026-03-17 15:36:04 +0100

    Added files

---
+++ yesnochatbot/yesnochatbot.py
@@ -0,0 +1,19 @@
+import random
+import time
+print("Aske me a yes or no question")
+text = input()
+running = True
+while running:
+
+    
+    
+    if input:
+        answer = random.randint(0,1)
+        if answer == 0:
+            print("No")
+            input()
+        if answer == 1:
+            print("Yes")
+            input()
+        
+    
